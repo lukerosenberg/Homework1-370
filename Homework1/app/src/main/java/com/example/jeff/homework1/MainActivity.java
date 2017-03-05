b8c5878ea8820b118fab02243134dcf8bd42ed17
Catherine Meyer <c93@catherinemeyer.org>
2017-03-05 10:26:11 -0800

    working code complete

--- Homework1/app/src/main/java/com/example/jeff/homework1/MainActivity.java
+++ Homework1/app/src/main/java/com/example/jeff/homework1/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.jeff.homework1;
 
+//import android.R;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Button;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity
+{
+
+    private Button nameButton;
+    private TextView nameText;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nameText = (TextView)findViewById(R.id.name_text);
+        nameButton = (Button)findViewById(R.id.name_button);
+
+        nameButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                MainActivity.this.nameText.setText(R.string.name_text);
+            }
+        });
     }
 }
