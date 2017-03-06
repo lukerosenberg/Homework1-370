08f01dc510127690d9d0ce407e9332f414269860
student <student@ssu-cs-dar25-167-150.sonoma.edu>
2017-03-05 17:26:04 -0800

    Started HW, but having trouble emulating on Android Studio

--- Homework1/app/src/main/java/com/example/jeff/homework1/MainActivity.java
+++ Homework1/app/src/main/java/com/example/jeff/homework1/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.jeff.homework1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private Button nameButton;
+    private TextView nameText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nameText= (TextView)findViewById(R.id.name_text);
+        nameButton= (Button)findViewById(R.id.name_button);
+        nameButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClickView(View v){
+                MainActivity.this.helloText.setText(R.string.name_text);
+            }
+        });
     }
 }
