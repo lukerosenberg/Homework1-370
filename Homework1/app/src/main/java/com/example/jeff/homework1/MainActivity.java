6c1cf5c289a1a55d9788515c473a809d767a01ff
student <student@ssu-cs-dar28-167-221.sonoma.edu>
2017-03-09 11:12:26 -0800

    working code complete

--- Homework1/app/src/main/java/com/example/jeff/homework1/MainActivity.java
+++ Homework1/app/src/main/java/com/example/jeff/homework1/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.jeff.homework1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button nameButton;
+    private TextView nameText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nameText=(TextView)findViewById(R.id.name_text);
+        nameButton=(Button)findViewById(R.id.button_text);
+        nameButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                MainActivity.this.nameText.setText(R.string.name_text);
+            }
+        });
     }
 }
